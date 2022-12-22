4c3208a5a3bd106fbf7e777c7db2c269264712da
Subham Dutta <subham.heritage@gmail.com>
2022-12-22 13:24:10 +0530

    validation custom message created

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const { register } = require('../controller/userController');
+const validationError = require('../helper/validationError')
 const Joi = require('joi');
 
 const router = [
@@ -14,18 +15,46 @@ const router = [
             validate: {
                 payload: Joi.object({
                     first_name: Joi.string()
+                        .alphanum()
+                        .min(3)
+                        .max(30)
                         .required()
-                        .description('First name of user'),
+                        .messages({
+                            "string.base": `"first_name" should be a type of 'text'`,
+                            "string.empty": `"first_name" cannot be an empty field`,
+                            "string.min": `"username" should have a minimum length of {#limit}`,
+                            "string.max": `"first_name" should have a maximum length of {#limit}`,
+                            "any.required": `"first_name" is a required field`
+                        }),
                     last_name: Joi.string()
+                        .alphanum()
+                        .min(3)
+                        .empty()
+                        .max(30)
                         .required()
-                        .description('Last name of user'),
+                        .messages({
+                            "string.base": `"last_name" should be a type of 'text'`,
+                            "string.empty": `"last_name" cannot be an empty field`,
+                            "string.min": `"last_name" should have a minimum length of {#limit}`,
+                            "string.max": `"last_name" should have a maximum length of {#limit}`,
+                            "any.required": `"last_name" is a required field`
+                          }),
                     email: Joi.string()
                         .required()
-                        .description('email of user'),
+                        .email({ tlds: { allow: false } })
+                        .description('valid email of user'),
                     password: Joi.string()
                         .required()
+                        .min(8)
+                        .max(20)
                         .description('password of user'),
-                })
+                    password_confirmation: Joi.any()
+                        .valid(Joi.ref('password'))
+                        .required()
+                        .description('Confirm password does not match'),
+                    
+                }),
+                failAction:validationError
             }
         }
     }, 
