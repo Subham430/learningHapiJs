6937bc24ba82840c274db67316a368e094cb1fb8
Subham Dutta <subham.heritage@gmail.com>
2022-12-27 17:25:23 +0530

    product update api ready

--- controller/productController/index.js
+++ controller/productController/index.js
@@ -1,5 +1,4 @@
 const {success, error} = require("../../response/macros.js");
-const bcrypt = require('bcrypt');
 
 //models
 const { Product } = require('../../models');
@@ -21,6 +20,23 @@ exports.all_products_details = async (request, h) => {
     try {
         const product_details = await Product.findAll();
           
+        return success({products: product_details}, "all product details fetched successfully", 200)(h);
+    } catch (err) {
+        return error({error: err.message})(h);
+    }
+};
+
+exports.update = async (request, h) => {
+    try {
+        const product_details = await Product.findOne({ where: { id: request.params.product_id } });
+        if(!product_details)
+            return error({error: "product doest not exists"})(h);
+        product_details.update({ 
+            product_name: request.payload.product_name,
+            description: request.payload.description,
+            price: request.payload.price
+         });
+
         return success({products: product_details}, "all product details fetched successfully", 200)(h);
     } catch (err) {
         return error({error: err.message})(h);
